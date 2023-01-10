19378e78ec00465dcd5a0643b813fe162f6543e0
Erik Wehrmann <erikwehrmann@Eriks-MacBook-Air.local>
2023-01-10 17:08:15 -0500

    day items and day list created and tested

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,24 @@ import React from "react";
 import "components/Application.scss";
 
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
   return (
     <main className="layout">
       <section className="sidebar">
---
+++ src/components/DayListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const formatSpots = function (spots) {
+    return spots >= 2 ? `${spots} spots remaining` : spots === 1 ? `${spots} spot remaining` : spots === 0 ? `no spots remaining` : '';
+  }
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
